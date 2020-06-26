2fe2187dff75bceea0a5d5658c73396501d3bd09
Segun Adelowo <segun.adelowo@interswitchgroup.com>
2020-06-26 20:45:03 +0100

    feat: Add airflow helpers

---
+++ Project-4 Data Pipelines with Airflow/plugins/helpers/sql_queries.py
@@ -0,0 +1,50 @@
+class SqlQueries:
+    songplay_table_insert = ("""
+        SELECT
+                md5(events.sessionid || events.start_time) songplay_id,
+                events.start_time, 
+                events.userid, 
+                events.level, 
+                songs.song_id, 
+                songs.artist_id, 
+                events.sessionid, 
+                events.location, 
+                events.useragent
+                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
+            FROM staging_events
+            WHERE page='NextSong') events
+            LEFT JOIN staging_songs songs
+            ON events.song = songs.title
+                AND events.artist = songs.artist_name
+                AND events.length = songs.duration
+    """)
+
+    user_table_insert = ("""
+        SELECT distinct userid, firstname, lastname, gender, level
+        FROM staging_events
+        WHERE page='NextSong'
+    """)
+
+    song_table_insert = ("""
+        SELECT distinct song_id, title, artist_id, year, duration
+        FROM staging_songs
+    """)
+
+    artist_table_insert = ("""
+        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+        FROM staging_songs
+    """)
+
+    time_table_insert = ("""
+        SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
+               extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
+        FROM songplays
+    """)
+    
+    song_table_count = ("SELECT COUNT(artistid) FROM songs WHERE artistid IS NULL")
+    
+    songplay_table_count = ("SELECT COUNT(songid) FROM songplays WHERE songid IS NULL")
+    
+    artist_table_count = ("SELECT COUNT(name) FROM artists WHERE name IS NULL")
+    
+    user_table_count = ("SELECT COUNT(level) FROM users WHERE level IS NULL")
